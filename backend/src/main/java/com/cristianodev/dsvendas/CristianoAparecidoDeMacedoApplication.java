58b6741456dfd3c8085091e8df1d2bda20f17a59
crisabcool <cristianoaparecido.abelhinha@gmail.com>
2021-05-06 16:10:55 -0300

    Projetos Criados

---
+++ backend/src/main/java/com/cristianodev/dsvendas/CristianoAparecidoDeMacedoApplication.java
@@ -0,0 +1,13 @@
+package com.cristianodev.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristianoAparecidoDeMacedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristianoAparecidoDeMacedoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cristianodev/dsvendas/CristianoAparecidoDeMacedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianodev.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristianoAparecidoDeMacedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
